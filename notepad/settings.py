e9ed5f4a4eff95271667842129ea6f6030253cb4
sirdesmond09 <sirdesmond09@gmail.com>
2021-08-17 15:42:43 +0100

    configure .env

--- notepad/settings.py
+++ notepad/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv('/home/notepad/notepad/notepad/.env')
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
